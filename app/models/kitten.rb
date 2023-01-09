d0c4f09a7dd2c219329b90e555d0bd030c6e6054
Wesley <matthew282@msn.com>
2023-01-09 12:41:26 -0700

    add kitten model

---
+++ app/models/kitten.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+
+# Table name: kittens
+
+# id                   :integer
+# name                 :string
+# cuteness             :integer
+# softness             :integer
+# created_at           :datetime
+# updated_at           :datetime
+
+class Kitten < ApplicationRecord
+
+end
