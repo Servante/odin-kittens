2e7f2f22dfab7072c31688741e7955ee7ba5f34d
Wesley <matthew282@msn.com>
2023-01-11 09:20:22 -0700

    fix typo

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -15,5 +15,5 @@ class Kitten < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true
   validates :cuteness, presence: true
-  validates :softness, prescence: true
+  validates :softness, presence: true
 end
