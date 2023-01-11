d18fde734eecb5702b46f943dd03a9f171a87f50
Wesley <matthew282@msn.com>
2023-01-11 09:21:45 -0700

    add #new and #create functionality

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -12,11 +12,24 @@ class KittensController < ApplicationController
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save  
+      redirect_to @kitten    
+    else
+      render :new, status: :unprocessable_entity
+    end   
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
